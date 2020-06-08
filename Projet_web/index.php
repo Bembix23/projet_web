4be102964bda1d0fe9dd6a199d7a9cfd55b94344
Benjamin Chancerel <benjiroux33@gmail.com>
2020-06-08 10:20:51 +0200

    reveille toi jules

--- Projet_web/index.php
+++ Projet_web/index.php
@@ -17,41 +17,46 @@
         <link rel="stylesheet" href="style.css" type="text/css">
     </head>
         <body>
-            <div id="index">
-                <div id="logo">
+            <div class="index">
+                <div class="logo">
                     <img src="logo.png"><h1>Firebee'n B</h1>
                 </div>
+                <div class="nav">
                     <a href="connexion.php" class = "link_home">Connexion</a>
                     <a href="inscription.php" class = "link_home">Inscription</a><br>
                     <a href="nous.php" class = "link_home">Qui sommes nous ?</a>
-                    <script type="text/javascript">
-                    const buttons = document.querySelectorAll('a');
-                    buttons.forEach(btn => {
-                        btn.addEventListener('click', function(e) {
+                    
+                </div>    
+                <div class="recherche">  
+                    <form action="recherche.php" method="post">
+                        <h2 class="ou">Ou voulez vous aller ?</h2>
+                        <input type="text" name="ville" placeholder="Ville" />
+                        <input type="number" name="nb_voyageur" placeholder="Nombre de voyageur" />
+                        <input type="date" name="Date_arrivé" placeholder="Date d'arrivé" /> 
+                        <input type="date" name="Date_départ" placeholder="Date de départ" /> 
+                        <input type="number" name="prix_max" placeholder="Budget maximum" />
+                        <button type="submit">Envoyer</button>
+                    </form>
+                </div>
+            </div> 
+            <script type="text/javascript">
+                const buttons = document.querySelectorAll('a');
+                buttons.forEach(btn => {
+                    btn.addEventListener('click', function(e) {
 
-                            let x = e.clientX - e.target.offsetLeft;
-                            let y = e.clientY - e.target.offsetTop;
+                        let x = e.clientX - e.target.offsetLeft;
+                        let y = e.clientY - e.target.offsetTop;
 
-                            let ripples = document.createElement('span');
-                            ripples.style.left = x + 'px';
-                            ripples.style.top = y + 'px';
-                            this.appendChild(ripples);
+                        let ripples = document.createElement('span');
+                        ripples.style.left = x + 'px';
+                        ripples.style.top = y + 'px';
+                        this.appendChild(ripples);
 
-                            setTimeOut(() => {
-                                ripples.remove()
-                            },1000);
-                        })
+                        setTimeOut(() => {
+                            ripples.remove()
+                        },1000);
                     })
-                    </script>
-            
-                <form action="recherche.php" method="post">
-                    <input type="text" name="ville" placeholder="Ville" />
-                    <input type="number" name="nb_voyageur" placeholder="Nombre de voyageur" />
-                    <input type="date" name="Date_arrivé" placeholder="Date d'arrivé" /> 
-                    <input type="date" name="Date_départ" placeholder="Date de départ" /> 
-                    <input type="number" name="prix_max" placeholder="Budget maximum" />
-                    <button type="submit">Envoyer</button>
-                </form>
-            </div>
+                })
+                </script>     
         </body>
     </html>
